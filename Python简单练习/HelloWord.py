97e48a748e2cd9dda42c4989ebfa7238db4a506b
xuyonghui6512 <1244885997@qq.com>
2018-01-14 16:34:33 +0800

    yy

---
+++ Python简单练习/HelloWord.py
@@ -0,0 +1,9 @@
+message="Hello Python World!"
+print(message)
+message="HEllo world!!"
+print(message)
+
+message='I have a "good" friend'
+print(message)
+message="I have a 'good' friend"
+print(message)
